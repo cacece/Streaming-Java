90eaa6a230d68abf41f4fea929b2476b2cd8bb2e
cacece <marcelodeandraderaposo@gmail.com>
2026-08-04 12:57:11 -0300

    Fix syntax error in Leitura.java and ensure proper input/output flow

--- src/Leitura.java
+++ src/Leitura.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 public class Leitura {
     public static void main(String[] args) {
-        Scanner leitura = new Scanner(System.in)
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito: ");
+        String filme = leitura.nextLine();
+        System.out.println("Qual o ano de lancamento? ");
+        int anoDeLancamento = leitura.nextInt();
+        System.out.println("Qual sua nota para o filme?");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
     }
 }
