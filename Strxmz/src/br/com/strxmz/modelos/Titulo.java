22edf4ec3c2490f2d790af847002e69bf62b71c1
cacece <marcelodeandraderaposo@gmail.com>
2026-09-02 16:25:36 -0300

    Refactor Filme class to extend Titulo

---
+++ Strxmz/src/br/com/strxmz/modelos/Titulo.java
@@ -0,0 +1,68 @@
+package br.com.strxmz.modelos;
+
+public class Titulo {
+private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lancamento: " + anoDeLancamento);
+        System.out.println("Incluido no plano: " + incluidoNoPlano);
+        System.out.println("Duração: " + duracaoEmMinutos + " minutos");
+        System.out.println("Total de avaliações: " + totalDeAvaliacoes);
+        System.out.println("Media das avaliacoes: " + pegaMedia());
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public double getSomaDasAvaliacoes() {
+        return somaDasAvaliacoes;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+   
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+}
